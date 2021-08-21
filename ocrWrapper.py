01c1d3f4f5a54a0fc4b9f3f8e36c4ef1da48a51f
Craig McMahon <cmcmahon@sgl.com>
2021-08-21 20:48:25 +0100

    Patch for locally install gocr binary

--- ocrWrapper.py
+++ ocrWrapper.py
@@ -1,5 +1,4 @@
 from PIL import Image
-import pytesseract
 import uuid
 import os
 
@@ -7,26 +6,14 @@ from subprocess import Popen, PIPE, call
 
 import cv2
 
-
-def tesseract(mat, config=None):
-    if config is None:
-        text = pytesseract.image_to_string(Image.fromarray(mat))
-    else:
-        text = pytesseract.image_to_string(Image.fromarray(mat), config=config)
-
-    return text
-
-
 def gocr(mat, encoding="utf-8"):
-    p = Popen(["gocr", "-"], stdin=PIPE, stdout=PIPE)
+    p = Popen(["gocr049.exe", "-"], stdin=PIPE, stdout=PIPE)
     retval, buf = cv2.imencode(".pgm", mat)
-
     p.stdin.write(buf)
     p.stdin.close()
     p.wait()
     text = p.stdout.read()
     p.stdout.close()
-
     return text.decode(encoding).strip("\n")
 
 
